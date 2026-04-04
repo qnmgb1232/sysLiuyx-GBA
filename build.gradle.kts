4b518b32c5db196117c52ecc2da34b8e993c1bab
sysLiux Developer <dev@sysliux.gba>
2026-04-04 21:59:54 +0800

    test: specify plugin version in app

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,1 @@
-plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-}
+// Empty - plugin versions specified in app/build.gradle.kts
